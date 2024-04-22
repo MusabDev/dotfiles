8119074102ee68c8f1f59f69c0f3d99b4bd0c2c3
Muhammad Musab <musabwebdev@gmail.com>
2024-04-22 16:55:22 +0600

    change discord rich presence

--- nvim/lua/user/plugins/discord-rich-presence.lua
+++ nvim/lua/user/plugins/discord-rich-presence.lua
@@ -1,11 +1,23 @@
 return {
+  -- {
+  --   'andweeb/presence.nvim',
+  --   config = function()
+  --     require('presence'):setup {
+  --       auto_update = true,
+  --       enable_line_number = true,
+  --       buttons = false,
+  --     }
+  --   end,
+  -- },
+
   {
-    'andweeb/presence.nvim',
+    'IogaMaster/neocord',
+    name = 'neocord',
+    event = 'VeryLazy',
     config = function()
-      require('presence'):setup {
-        auto_update = true,
-        enable_line_number = true,
-        buttons = false,
+      require('neocord').setup {
+        main_image = 'logo',
+        logo = 'https://www.vim.org/images/vim_4star.gif',
       }
     end,
   },
