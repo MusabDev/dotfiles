acbe9720aee230f98450e2fa7c44f1e4f9a79557
Muhammad Musab <musabwebdev@gmail.com>
2024-12-15 06:33:18 +0600

    New nvim config

---
+++ nvim.old/init.lua
@@ -0,0 +1,24 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'user.lazy'
+require 'user.options'
+require 'user.keymaps'
+require 'user.colors'
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
